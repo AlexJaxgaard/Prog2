57d4d75e417aae7d6c81b450f2a7f5deaa1738c2
Alexander Jaxgård <snalexdci@hotmail.com>
2023-05-03 13:00:11 +0200

    aaa

---
+++ Prog2/Edge.java
@@ -0,0 +1,37 @@
+import java.io.Serializable;
+
+public class Edge<T> implements Serializable {
+
+    private final T destination;
+    private int weight;
+    private final String name;
+
+    public Edge(T destination, int weight, String name){
+        this.destination = destination;
+        this.weight = weight;
+        this.name = name;
+    }
+
+
+    public T getDestination(){
+        return this.destination;
+    }
+
+    public int getWeight(){
+        return this.weight;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+     public void setWeight(int newWeight){
+        this.weight = newWeight;
+    }
+
+    @Override
+    public String toString(){
+        return "";
+    }
+
+}
