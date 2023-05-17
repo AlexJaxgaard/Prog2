f5590fcd9681de7cc469e44793b1accb3ea8a0b0
Alexander Jaxgård <snalexdci@hotmail.com>
2023-05-17 14:24:40 +0200

    Update PathFinder.java

--- Prog2/src/PathFinder.java
+++ Prog2/src/PathFinder.java
@@ -1,9 +1,11 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -20,18 +22,36 @@ public class PathFinder<T> extends Application {
         root.setCenter(center);
 
         Menu menu = new Menu("File");
-        MenuItem menuItem1 = new MenuItem("New Map");
-        MenuItem menuItem2 = new MenuItem("Open");
-        MenuItem menuItem3 = new MenuItem("Save");
-        MenuItem menuItem4 = new MenuItem("Save image");
-        MenuItem menuItem5 = new MenuItem("Exit");
+        MenuItem newMap = new MenuItem("New Map");
+        MenuItem open = new MenuItem("Open");
+        MenuItem save = new MenuItem("Save");
+        MenuItem saveImage = new MenuItem("Save image");
+        MenuItem exit = new MenuItem("Exit");
 
-        menuItem1.setOnAction(openFileHandler);
+        newMap.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                javafx.scene.image.Image image = new javafx.scene.image.Image(
+                        "file:C:\\Users\\snale\\Documents\\GitHub\\Prog2\\Prog2\\src\\europa.gif");
+                ImageView imageView = new ImageView(image);
+                root.setCenter(imageView);
+            }
+        });
 
-
+        menu.getItems().addAll(newMap, open, save, saveImage);
         MenuBar menuBar = new MenuBar();
         menuBar.getMenus().add(menu);
 
+
+
+        Scene scene = new Scene(root);
+
+        root.setTop(menuBar);
+
+
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static class openFileHandler implements EventHandler<ActionEvent> {
